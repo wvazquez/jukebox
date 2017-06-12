10ea7e1ab5186986ff8556f476fa819d511ceac8
wvazquez <vazquezwilmer@gmail.com>
2017-06-12 02:05:12 -0400

    Updates to the modal window

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(".search").on('click', function(){
   if($(".searchbox").val().trim().length === 0){ return; }
+  $('.search').attr('data-target', "#myModal");
   searchTrack($(".searchbox").val());
+
 });
+
 $("#list").on('click', 'a', function(){
   var song = $(this).text().split(' ').pop();
   var song_id = song.substring(1,song.length-1);
   setTrack(song_id, $(this));
 });
-
+var result;
 function searchTrack(name){
   $.ajax({
     type: "GET",
@@ -19,7 +22,7 @@ function searchTrack(name){
     dataType: 'json',
     success: function(response) {
         console.log(response);
-        var result = response.aRows;
+        result = response.aRows;
         $('#list li').replaceWith('');
         for(var i=0; i< result.length; i++){
           $('#list').append("<li><a class ='the_song' href='#'>" + result[i]+ "</a></li>");
@@ -43,15 +46,40 @@ function setTrack(song_id, listItem){
         var track = response.dataset[0];
         $('#song source').attr('src', "https://files.freemusicarchive.org/" + track.track_file);
         $('#song').load();
-        $('.current-song img').replaceWith('');
-        $('.current-song').prepend('<img src="'+ track.track_image_file + '" class = "cover-image" alt="Track Cover Image">');
-
+        if(track.track_image_file === ""){
+          $('.cover-image').attr('src', "../images/headphones1.jpg");
+        }else{
+          $('.cover-image').attr('src', track.track_image_file);
+        }
         $('.current-song h4').replaceWith('');
         $('.current-song').prepend('<h4 class="track_title">'+ track.track_title.toUpperCase() + '</h4>');
-
       },
       error: function(err) {
           console.error(err);
       }
   });
 }
+
+
+
+
+
+
+function Jukebox(){
+  this.play = function(){
+    song.play();
+  };
+  this.pause = function(){
+    song.pause();
+  };
+};
+
+var mySong = new Jukebox();
+var song = $('#song')[0];
+
+$('#play').on("click",function(){
+    song.play();
+});
+$('#pause').on("click",function(){
+    song.pause();
+});
