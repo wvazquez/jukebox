030d55b585e2c7525cf5114b8941709c9a5ebdba
wvazquez <vazquezwilmer@gmail.com>
2019-09-03 15:15:26 -0400

    Sets up handlebars partials and layouts.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const hbs = require('express-handlebars');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine( 'hbs', hbs( { 
+  extname: 'hbs', 
+  defaultLayout: 'main', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
